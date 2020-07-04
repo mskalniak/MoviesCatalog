51802289acc7f871b3f9a93a84382f273f158cc6
Mateusz Skalniak <mskalniak@gmail.com>
2020-07-04 22:26:16 +0200

    Favourites panel added + adverts

--- app.js
+++ app.js
@@ -1,40 +1,109 @@
+console.log('Movies Catalog app');
 
-document.addEventListener("DOMContentLoaded", function() {
-    console.log('COUNTER app');
+let movieName = 'Last dance';
+let favouritesMovies = [];
+let lastId = 0;
+let counter = 0;
+updateCounter();
 
-    console.log(document);
-    
-    const containerElem = document.getElementById('container');
-    console.log(containerElem);
-    
-    const paragraphElem = document.createElement('p');
-    paragraphElem.innerText = "To jest tekst!";
-    paragraphElem.innerHTML = "<button id='btn'>button</button>";
-    paragraphElem.id = "pid";
-    paragraphElem.classList.add('active');
-    paragraphElem.style.background = 'green';
-    paragraphElem.style.borderTop = '10px solid blue';
-    
-    console.log(paragraphElem);
-    
-    containerElem.appendChild(paragraphElem);
-    
-    console.log(containerElem.getElementsByTagName('p'));
-    containerElem.getElementsByClassName('active')
-    console.log(containerElem.querySelector('#container p'));
-    
-    
-    const buttonElem = document.querySelector('#btn');
-    console.log(buttonElem);
-    
-    buttonElem.addEventListener('scroll', function() {
-        console.log('button clicked!');
-    });
-    
-    //mouseover
-    //mouseout
-    //dblclick
-    //scroll
-    
+const adverts = [
+    'Nowy film: Zielona Mila, w kinach od 22 września!',
+    'Zapraszamy do kin już od 23 czerwca!',
+    'Festiwal filmowy już od poniedziałku w kinie Burleska!',
+    'Filmowy wieczór z kinem Adriatyk już dziś!',
+    'Brakujące ogniwo to nowy film Scorsese, nie możesz go przegapić!'
+];
+let currentAdvert = adverts[0];
+updateAdvertsUiElem();
 
-});
+startAdverts();
+
+const favouriteInputElem = document.getElementById('favouriteInput');
+favouriteInputElem.value = movieName;
+
+const favouritesListElem = document.getElementById('favouritesList');
+
+function onAddClicked() {
+    const favouriteInputElem = document.getElementById('favouriteInput');
+
+    //model
+    movieName = favouriteInputElem.value;
+
+    let movie = {
+        id: lastId++,
+        name: movieName
+    }
+    favouritesMovies.push(movie);
+
+    //ui
+    const fvElem = createFavouriteMovieUiElem(movie);
+    favouritesListElem.appendChild(fvElem);
+    updateCounter();
+}
+
+function updateCounter() {
+    const favouritesCounterElem = document.getElementById('favouritesCounter');
+
+    //model
+    counter = favouritesMovies.length;
+
+    //ui
+    favouritesCounterElem.innerText = counter;
+}
+
+function createFavouriteMovieUiElem(movie) {
+    const divElem = document.createElement('div');
+    divElem.id = `movie-${movie.id}`;
+    divElem.classList.add('mc-margin-10');
+    const pElem = document.createElement('span');
+    const btnElem = document.createElement('button');
+    
+    btnElem.id = `${movie.id}`;
+    btnElem.innerText = 'Usuń';
+    btnElem.style.setProperty('float', 'right');
+
+    btnElem.addEventListener('click', onRemoveClicked);
+
+    pElem.innerText = movie.name;
+    divElem.appendChild(pElem);
+    divElem.appendChild(btnElem);
+
+    return divElem;
+}
+
+function onRemoveClicked(event) {
+    const idToDelete = Number(event.target.id);
+
+    //model
+    favouritesMovies = favouritesMovies.filter(x => x.id !== idToDelete);
+
+    //ui
+    const uiElemToDel = document.getElementById(`movie-${idToDelete}`);
+    favouritesListElem.removeChild(uiElemToDel);
+    updateCounter();
+}
+
+function startAdverts() {
+    setInterval(function() {
+        const randomId = parseInt(Math.random() * 10 % adverts.length);
+
+        //model
+        currentAdvert = adverts[randomId];
+
+        //ui
+        updateAdvertsUiElem();
+    }, 1500);
+}
+
+function updateAdvertsUiElem() {
+    const advertsElem = document.getElementById('adverts');
+    advertsElem.innerText = currentAdvert;
+}
+
+function onSearchClicked() {
+    fetch('http://www.omdbapi.com/?apikey=7015f6d&t=last+dance').then(function(response) {
+        return response.json();
+    }).then(function(result) {
+        console.log(result);
+    })
+}
